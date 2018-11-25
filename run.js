e439fcfad3ea0ad98299a18295e91e7339428872
MattMorgis <matt@rappiddevelopment.com>
2018-11-25 01:40:57 -0500

    feat: first run

--- run.js
+++ run.js
@@ -1,10 +1,12 @@
+const fs = require("fs");
 const {URL} = require("url");
-const request = require("request-promise-native");
+const {map} = require("arrasync");
+const got = require("got");
 
 const PACKAGE_NAME = "request";
 
+// https://github.com/npm/registry/blob/master/docs/REPLICATE-API.md
 const dependentPackagesByPage = (package, page) => {
-  // https://github.com/npm/registry/blob/master/docs/REPLICATE-API.md
   const registry = new URL(
     "/registry/_design/app/_view/dependedUpon",
     "https://skimdb.npmjs.com"
@@ -21,7 +23,7 @@ const dependentPackagesByPage = (package, page) => {
 };
 
 // https://github.com/npm/registry/blob/master/docs/download-counts.md#point-values
-const packageDownloads = package => {
+const packageDownloadsURL = package => {
   return new URL(
     "/downloads/point/last-month/" + package,
     "https://api.npmjs.org"
@@ -32,51 +34,69 @@ async function* getDependentPackagesByBatches(package, batches = 1000) {
   // [0, 1, 2, 3 ... pageLimit] slice (remove 0) -> [1, 2, 3, ... pageLimit]
   const numberOfPagesToFetch = [...new Array(batches + 1).keys()].slice(1);
   for (const page of numberOfPagesToFetch) {
+    process.stdout.write("requesting batch " + page + " of " + batches + "\r");
+    process.stdout.write("\n");
+    //console.log("requesting batch " + page + " of " + batches);
     try {
-      const packages = await request(dependentPackagesByPage(package, page), {
+      const packages = await got(dependentPackagesByPage(package, page), {
         json: true
       });
-      const packageNames = packages.rows.map(item => item.key[1]);
-      yield packageNames;
+      yield packages.body.rows;
     } catch (e) {
       throw e;
     }
   }
 }
 
-async function* getPackageFromBatch(batch) {
-  for await (const packages of batch) {
-    for (const package of packages) {
-      yield package;
-    }
+async function* getPackageDownloads(batches) {
+  let batch = 0;
+  for await (const packages of batches) {
+    batch++;
+    const packageDownloads = await map(packages, async p => {
+      try {
+        process.stdout.write(
+          "requesting downloads for package " + p.key[1] + "\r"
+        );
+
+        const downloadStats = await got(packageDownloadsURL(p.key[1]), {
+          json: true
+        });
+        const {downloads, package} = downloadStats.body;
+        return {downloads, package};
+      } catch (e) {
+        if (e.statusCode === 404) {
+          return null;
+        }
+        throw e;
+      }
+    });
+    process.stdout.write("\n");
+    yield packageDownloads.filter(e => e !== null);
   }
 }
 
-async function* getDownloadsForPackage(pkg) {
-  for await (const package of pkg) {
-    try {
-      const downloadStats = await request(packageDownloads(package), {
-        json: true
-      });
-      yield downloadStats;
-    } catch (e) {
-      throw e;
+const writeCSV = async batch => {
+  const fileStream = fs.createWriteStream(__dirname + "/results.csv");
+  try {
+    for await (const packages of batch) {
+      for (package of packages) {
+        if (package.package !== undefined || package.downloads !== undefined) {
+          fileStream.write(package.package);
+          fileStream.write(", ");
+          fileStream.write(package.downloads.toString());
+          fileStream.write("\n");
+        }
+      }
     }
-  }
-}
-
-const log = async lines => {
-  for await (const line of lines) {
-    console.log(line);
+  } catch (e) {
+    throw e;
   }
 };
 
 const main = async () => {
   try {
-    log(
-      getDownloadsForPackage(
-        getPackageFromBatch(getDependentPackagesByBatches(PACKAGE_NAME, 500))
-      )
+    writeCSV(
+      getPackageDownloads(getDependentPackagesByBatches(PACKAGE_NAME, 1000))
     );
   } catch (e) {
     console.log(e.message);
